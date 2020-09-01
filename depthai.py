a9b703e8073e7c5d9538543de44c705178ee48db
alex-luxonis <alex@luxonis.com>
2020-09-01 23:31:36 +0300

    Add camera 3A controls

--- depthai.py
+++ depthai.py
@@ -512,6 +512,19 @@ while True:
         depthai.request_af_mode(depthai.AutofocusMode.AF_MODE_AUTO)
     elif key == ord('2'):
         depthai.request_af_mode(depthai.AutofocusMode.AF_MODE_CONTINUOUS_VIDEO)
+    # 5,6,7,8,9,0 : temporary, TODO delete
+    elif key == ord('5'):
+        depthai.send_isp_3a_command(depthai.Cam3A.Left, depthai.Isp3A.CMD_AE_REGION, '0 0 200 200 1')
+    elif key == ord('6'):
+        depthai.send_isp_3a_command(depthai.Cam3A.Left, depthai.Isp3A.CMD_AE_REGION, '1000 0 200 200 1')
+    elif key == ord('7'):
+        depthai.send_isp_3a_command(depthai.Cam3A.Left, depthai.Isp3A.CMD_EXPOSURE_COMPENSATION, '-2')
+    elif key == ord('8'):
+        depthai.send_isp_3a_command(depthai.Cam3A.Left, depthai.Isp3A.CMD_EXPOSURE_COMPENSATION, '+2')
+    elif key == ord('9'):
+        depthai.send_isp_3a_command(depthai.Cam3A.Right, depthai.Isp3A.CMD_EXPOSURE_COMPENSATION, '-2')
+    elif key == ord('0'):
+        depthai.send_isp_3a_command(depthai.Cam3A.Right, depthai.Isp3A.CMD_EXPOSURE_COMPENSATION, '+2')
     elif key == ord('q'):
         break
 
